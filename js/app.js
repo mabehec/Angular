3e3cc57d791581aa43d82debb28ec82e4f68ba6b
Mathieu <mathieu@icible.ca>
2014-08-12 14:57:10 -0400

    Comm

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var app = angular.module('ic_billetterie',[
 
-    'ic_controleur'
+    'ic_controleur',
+    'ic_usine'
 ]);
 
---
+++ js/factory.js
@@ -0,0 +1,5 @@
+var usine = angular.module('ic_usine',[])
+
+usine.factory =('ic_factoryTest', function(){
+
+})
