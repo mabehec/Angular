cbe9374df4fd75c7906fac4a907f9b17f02e6075
mabehec <mabehec@gmail.com>
2014-08-15 15:37:20 -0400

    t

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var app = angular.module('ic_billetterie',[
 
     'ic_controleur',
     'ic_usine'
-])
+]);
 
 app.value('$',$);
 
--- js/controller.js
+++ js/controller.js
@@ -5,3 +5,12 @@ controleur.controller('testCtrl', function($scope){
 
 });
 
+
+controleur.controller('signalar', function($scope){
+    var commandsHub = $.connection.commandsHub
+    var commandId="d4768108-49b0-46e5-8a77-c7bc327fb626"
+    $scope.majstatut = function (commandStatus){commandsHub.client.updateStatus(commandStatus)}
+    $scope.monitor = function(commandId){commandsHub.server.monitorCommand(commandId)}
+
+});
+
